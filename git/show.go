eb8d6a3b400f930d13f19b74a1becb9307258a53
Thomas Gardner <tom@faff.stream>
2024-07-08 11:41:05 +1000

    git.Show: redirect /.well-known/* URIs to /well-known/* refs
    Git won't create refs with a leading dot. This commit lets us do things
    like, eg, serve an AT Protocol DID.

--- git/show.go
+++ git/show.go
@@ -7,15 +7,22 @@ import (
 	"strings"
 )
 
-const errNotFound = "this is not the blob you're looking for"
+const (
+	errNotFound = "this is not the blob you're looking for"
+	wellKnown   = ".well-known/"
+)
 
 func Show(path, defaultBranch, commitFormat string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slash := strings.Index(r.URL.Path, "/")
 		id := r.URL.Path[slash+1:]
+		f := commitFormat
 		switch {
 		case id == "":
 			id = defaultBranch
+		case len(wellKnown) < len(id) && id[:len(wellKnown)] == wellKnown:
+			f = "%B"
+			id = id[1:]
 		case id[0] == '-':
 			w.WriteHeader(http.StatusNotFound)
 			w.Write([]byte(errNotFound))
@@ -23,7 +30,7 @@ func Show(path, defaultBranch, commitFormat string) func(w http.ResponseWriter, 
 		// TODO: send an error for files created by `git init --bare`
 		default:
 		}
-		cmd := exec.Command("git", "-C", path, "show", "--format="+commitFormat, id)
+		cmd := exec.Command("git", "-C", path, "show", "--format="+f, id)
 		cmd.Stdout = w
 		if err := cmd.Run(); err != nil {
 			w.WriteHeader(http.StatusNotFound)
